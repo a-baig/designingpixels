67d1e4aa8d7ca2e2497fdc2698899c62ef1d121e
a-baig <112501920+a-baig@users.noreply.github.com>
2022-09-02 10:58:43 +0500

    Add files via upload

---
+++ Mountees.js
@@ -0,0 +1,11 @@
+
+function openmenu() {
+    navigation = document.getElementById('nav-b');
+if (navigation.style.display == "none"){
+    navigation.style.display = "flex ";
+}
+else{
+    navigation.style.display = "none ";
+}
+    
+}
